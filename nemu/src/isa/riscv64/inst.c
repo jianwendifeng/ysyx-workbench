165b43bf804caa5081c9a6d2bb97081785235ee6
tracer-ysyx <tracer@ysyx.org>
2023-05-17 16:44:57 +0800

    > compile NEMU ysyx_22050280 袁永强 Linux todas-virtual-machine 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:44:57 up 10:42,  1 user,  load average: 0.24, 0.22, 0.23

--- nemu/src/isa/riscv64/inst.c
+++ nemu/src/isa/riscv64/inst.c
@@ -33,7 +33,7 @@ enum {
 #define immU() do { *imm = SEXT(BITS(i, 31, 12), 20) << 12; } while(0)
 #define immS() do { *imm = (SEXT(BITS(i, 31, 25), 7) << 5) | BITS(i, 11, 7); } while(0)
 #define immJ() do { *imm = (SEXT(BITS(i, 31, 31) ,1) << 20) | (BITS(i, 19, 12) << 12) | (BITS(i, 20, 20) << 11)|(BITS(i, 30, 21) << 1); } while(0)
-#define immB() do { *imm = ((SEXT(BITS(i, 31, 31) ,1) << 12) | (BITS(i, 7, 7) << 11) | (BITS(i, 30, 25) << 5) | (BITS(i, 11, 8) << 1)) << 1; } while(0)
+#define immB() do { *imm = (SEXT(BITS(i, 31, 31) ,1) << 12) | (BITS(i, 7, 7) << 11) | (BITS(i, 30, 25) << 5) | (BITS(i, 11, 8) << 1); } while(0)
 
 
 static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2, word_t *imm, int type) {
@@ -95,7 +95,7 @@ static int decode_exec(Decode *s) {
 	INSTPAT("0000000 ????? ????? 010 ????? 01100 11", sltiu  , I, R(rd) = ((unsigned int)src1 < (unsigned int)imm) ? 1 : 0 );
   //INSTPAT("0000000 ????? ????? 001 ????? 01100 11", sll  , R, INV(s->pc));
   //INSTPAT("??????? ????? ????? ??? ????? 01100 11", srl  , R, INV(s->pc));
-  INSTPAT("0100000 ????? ????? 000 ????? 01100 11", sub    , R, R(rd) = src1 - src2);
+  INSTPAT("0100000 ????? ????? 000 ????? 01100 11", sub    , R, R(rd) = src1 - src2 );
   //INSTPAT("??????? ????? ????? ??? ????? 01100 11", sra  , R, INV(s->pc));
 
 	INSTPAT("??????? ????? ????? 010 ????? 01000 11", sw     , S, Mw(src1 +imm, 4, src2));
