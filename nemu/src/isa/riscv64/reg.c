1103da8eed640c45c0e43e5271a92b2001d1abf7
tracer-ysyx <tracer@ysyx.org>
2023-05-28 20:58:36 +0800

    > compile NEMU ysyx_22050280 袁永强 Linux todas-virtual-machine 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:58:36 up 2 days, 11:25,  1 user,  load average: 0.14, 0.25, 0.24

--- nemu/src/isa/riscv64/reg.c
+++ nemu/src/isa/riscv64/reg.c
@@ -27,7 +27,7 @@ void isa_reg_display() {
 	int i;
 	 for (i = 0; i < 32; i ++)
 	 {
-		 printf ("%s\t\t0x%-20lx\t	%ld\n",regs[i],cpu.gpr[i],cpu.gpr[i]);
+		 printf ("%s\t\t0x%-20lx\t\t%ld\n",regs[i],cpu.gpr[i],cpu.gpr[i]);
 	 } 
 	 printf ("%s\t\t0x%lx\n","pc", cpu.pc);
 }
