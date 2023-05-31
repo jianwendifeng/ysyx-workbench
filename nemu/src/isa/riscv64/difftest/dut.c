d6fe3576f7bf9b27259ea5f7ebf4764f1c910327
tracer-ysyx <tracer@ysyx.org>
2023-05-31 20:25:03 +0800

    > compile NEMU ysyx_22050280 袁永强 Linux todas-virtual-machine 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:25:03 up 2 days, 22:10,  1 user,  load average: 0.16, 0.19, 0.18

--- nemu/src/isa/riscv64/difftest/dut.c
+++ nemu/src/isa/riscv64/difftest/dut.c
@@ -21,8 +21,9 @@ bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc)
 {
 	//word_t ref_pc = ref_r->pc;
 	printf("ref_pc: %lx,pc: %lx/n",ref_r->pc-4,pc);
- 	if(ref_r->pc-4 == pc){
-	return false;}
+ 	if(ref_r->pc-4 != pc){
+	return false;
+	}
 	return true;
 }
 
