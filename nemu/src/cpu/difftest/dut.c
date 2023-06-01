c5016a7fe924e9b5af0b6ee32d0e1f51e5de8483
tracer-ysyx <tracer@ysyx.org>
2023-06-01 17:17:25 +0800

    > compile NEMU ysyx_22050280 袁永强 Linux todas-virtual-machine 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:17:25 up 3 days,  5:04,  1 user,  load average: 0.20, 0.21, 0.23

--- nemu/src/cpu/difftest/dut.c
+++ nemu/src/cpu/difftest/dut.c
@@ -107,6 +107,7 @@ void difftest_step(vaddr_t pc, vaddr_t npc) {
     ref_difftest_regcpy(&ref_r, DIFFTEST_TO_DUT);
     if (ref_r.pc == npc) {
       skip_dut_nr_inst = 0;
+			panic("ref_r:= "	FMT_WORD	"npc:%lx\t\n",ref_r.pc,npc);
       checkregs(&ref_r, npc);
       return;
     }
