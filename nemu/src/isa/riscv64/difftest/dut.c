f254da2eae059343c998ba51239d3842d12e0abd
tracer-ysyx <tracer@ysyx.org>
2023-06-01 19:39:00 +0800

    > compile NEMU ysyx_22050280 袁永强 Linux todas-virtual-machine 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:39:00 up 3 days,  7:26,  1 user,  load average: 0.25, 0.28, 0.26

--- nemu/src/isa/riscv64/difftest/dut.c
+++ nemu/src/isa/riscv64/difftest/dut.c
@@ -25,7 +25,7 @@ bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc)
 			//printf("%dreg_name:\t%ld:\t%#lx\t\tpc_name:\t%ld\t%#lx\n",i,ref_r->gpr[i],ref_r->gpr[i],cpu.gpr[i],cpu.gpr[i]);
 			if(ref_r->pc != pc ){
 				//printf("Wrong pc:%#lx\tRight pc:%#lx\nWrong reg value:%#lx\tRight value:%#lx\n",ref_r->pc,ref_r->pc,cpu.gpr[i],ref_r->gpr[i]);
-				printf("Wrong pc:%#lx\tRight pc:%#lx\nWrong reg value:%#lx\tRight value:%#lx\n",ref_r->pc,pc,cpu.gpr[i],ref_r->gpr[i]);
+			printf("Wrong pc:%#lx\tRight pc:%#lx\n",ref_r->pc,pc);
 				
 				//isa_reg_display();
 			return false;
