eec4230a1251aec8175f028c7537e962699d0b6b
tracer-ysyx <tracer@ysyx.org>
2023-06-09 21:12:35 +0800

    > compile NEMU ysyx_22050280 袁永强 Linux todas-virtual-machine 5.19.0-43-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon May 22 13:39:36 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:12:35 up 17:25,  1 user,  load average: 0.96, 0.53, 0.36

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -61,8 +61,8 @@ int strncmp(const char *s1, const char *s2, size_t n) {
   while(i !=  n){
     if(s1[i] < s2[i]) return -1;
     else if(s1[i] > s2[i]) return 1;
-		else if(s1[i] == '\0' || s2[i] == '\0') return 0;
-		i++;
+		else if(s1[i] == '\0') return 0;
+		
   }
   return 0;
 }
