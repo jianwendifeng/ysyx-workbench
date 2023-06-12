a99778cc044ebf105339c532394f5c05e3fd922e
tracer-ysyx <tracer@ysyx.org>
2023-06-12 20:06:29 +0800

    > compile NEMU ysyx_22050280 袁永强 Linux todas-virtual-machine 5.19.0-43-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon May 22 13:39:36 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:06:29 up 1 day, 10:23,  1 user,  load average: 0.80, 0.56, 0.47

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -5,6 +5,25 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
+void reverse(char s[]) {
+    int i, j;
+    char temp;
+    
+    for (i = 0, j = strlen(s)-1; i < j; i++, j--) {
+        temp = s[i];
+        s[i] = s[j];
+        s[j] = temp;
+    }
+}
+
+void itoa(int n,char s[]){
+       int i=0;
+       do{
+               s[i++] = n %10 +'0';
+       }while((n /= 10) > 0);
+       reverse(s);
+}
+
 int printf(const char *fmt, ...) {
   panic("Not implemented");
 }
@@ -15,27 +34,47 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 
 int sprintf(char *out, const char *fmt, ...) {
 	va_list ap;
-	va_start(ap, fmt);	//把参数列表拷贝到ap中
-  int i=0;
-  char* buf = out;
-  while(*fmt != '\0')
+  va_start(ap, fmt);      //把参数列表拷贝到ap中,
+  int len=0;
+  int arg_i;
+  char arg_i_s[1024];
+  char *arg_s;
+  int i = 0;
+
+  while(fmt[i] != '\0')
   {
-    if(*fmt == '%')
+    if(fmt[i] == '%')
     {
-      fmt++;  //指针移动到%后面
+      i++;  //指针移动到%后面
+    
+      switch(fmt[i])
+      {
+        case 's': {
+           arg_s = va_arg(ap,char*);
+            for(int j = 0 ; arg_s[j] != '\0' ; j++){
+            out[len++] = arg_s[j++];
+            }
+             break;
+         }
+       case 'd': {
+           arg_i = va_arg(ap,int);  
+           itoa(arg_i,arg_i_s);
+           for(int j = 0 ; arg_i_s[j] != '\0' ; j++){
+             out[len++] = arg_i_s[j++];
+           }
+           break;
+         }
+       default:{
+         out[len++] = fmt[i++];
+       }
+      }
+    } else{
+      out[len++] = fmt[i++];
     }
-    switch(*fmt)
-    {
-      case 's': *buf = *va_arg(ap,char*); break;
-      case 'd': *buf = va_arg(ap,int);  break;
-      default:panic("Sprintf this type haven't implemented.");
-    }
-    i++;
-    buf++;
-    fmt++;
   }
+  out[len] = '\0';
   va_end(ap);
-  return i;
+  return len;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
