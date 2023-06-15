e3a1ae6c7d75edaae1b4885b2d62e52cf3cf5e71
tracer-ysyx <tracer@ysyx.org>
2023-06-15 10:37:05 +0800

    > sim RTL ysyx_22050280 袁永强 Linux todas-virtual-machine 5.19.0-43-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon May 22 13:39:36 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:37:05 up 57 min,  1 user,  load average: 0.44, 0.32, 0.30

--- npc/vsrc/exu.v
+++ npc/vsrc/exu.v
@@ -1,4 +1,4 @@
-//`define OP_ADDI 7'b0010011
+`define OP_ADDI 7'b0010011
 //`define FUNCT3_ADDI 3'b000
 
 module exu(
@@ -13,7 +13,7 @@ module exu(
 
 	always @(*) begin
 	case (ex_op)
-		7'b0010011 : begin
+		`OP_ADDI : begin
 			case (ex_funct3)
 				3'b000:  ex_result = ex_src1 + {20'b0,ex_imm};	//clk use <= ,this not have clk
 				default: ex_result = 32'b0;
