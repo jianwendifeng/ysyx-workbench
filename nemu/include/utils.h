aacb572a0cd98d1c20edc1ff88a95d389af33099
tracer-ysyx <tracer@ysyx.org>
2023-07-12 11:52:35 +0800

    > compile NEMU ysyx_22050280 袁永强 Linux todas-virtual-machine 5.19.0-43-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon May 22 13:39:36 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:52:35 up 16:46,  1 user,  load average: 0.23, 0.29, 0.32

--- nemu/include/utils.h
+++ nemu/include/utils.h
@@ -58,10 +58,10 @@ uint64_t get_time();
 
 #define log_write(...) IFDEF(CONFIG_TARGET_NATIVE_ELF, \
   do { \
-    extern FILE* log_fp; printf("itrace\n");\
+    extern FILE* log_fp; \
     extern bool log_enable(); \
     if (log_enable()) { \
-      fprintf(log_fp, __VA_ARGS__); int i = 0;printf("No.%d",i++);\
+      fprintf(log_fp, __VA_ARGS__); \
       fflush(log_fp); \
     } \
   } while (0) \
