1cad64689d891aa2d6ebc28f27cb61534ecf5c75
tracer-ysyx <tracer@ysyx.org>
2023-07-13 10:59:26 +0800

    > compile NEMU ysyx_22050280 袁永强 Linux todas-virtual-machine 5.19.0-46-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jun 21 15:35:31 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:59:26 up  1:31,  1 user,  load average: 0.40, 0.31, 0.29

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -60,7 +60,7 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
   if (ITRACE_COND) { log_write("%s\n", _this->logbuf); //itrace
     write_iringbuf(_this);  //iringbuf
   }  
-  if (nemu_state.state != NEMU_RUNNING) { read_iringbuf(); }  //output iringbuf
+  //if (nemu_state.state != NEMU_RUNNING) { read_iringbuf(); }  //output iringbuf
 #endif
   if (g_print_step) { IFDEF(CONFIG_ITRACE, puts(_this->logbuf)); }
   IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
