a9e9eba9f29e0278a685e4b95179a37bb9e8fe03
tracer-ysyx <tracer@ysyx.org>
2023-07-13 11:05:07 +0800

    > compile NEMU ysyx_22050280 袁永强 Linux todas-virtual-machine 5.19.0-46-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jun 21 15:35:31 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:05:07 up  1:37,  1 user,  load average: 0.28, 0.28, 0.27

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -50,7 +50,7 @@ void read_iringbuf(){
   while((i--) != iringbuf.num)
   {
     printf ("%#lx\t\t%s\t\t\n",iringbuf.instr[i%16].pc,iringbuf.instr[i%16].logbuf);
-    printf("\n");
+    printf("%d\n",i);
   }
 }
 
