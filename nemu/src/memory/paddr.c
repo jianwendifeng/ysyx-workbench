93549d0f708c99c520dd8c917caf5a1f738991f0
tracer-ysyx <tracer@ysyx.org>
2023-07-14 21:24:58 +0800

    > compile NEMU ysyx_22050280 袁永强 Linux todas-virtual-machine 5.19.0-46-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jun 21 15:35:31 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:24:58 up  2:25,  1 user,  load average: 0.48, 0.45, 0.38

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -56,10 +56,12 @@ void init_mem() {
   Log("physical memory area [" FMT_PADDR ", " FMT_PADDR "]", PMEM_LEFT, PMEM_RIGHT);
 }
 
-// extern void read_iringbuf();
-// int pri = 0;
+//  extern void read_iringbuf();
+extern void isa_reg_display();
+ int pri = 0;
 word_t paddr_read(paddr_t addr, int len) {
-  //if(pri == 449) read_iringbuf();
+  pri++;
+  if(pri > 449 && pri < 450) isa_reg_display();
   //printf("NO.paddr_read%d\tRead_addr  = %#x\n",pri++,addr);
   if (likely(in_pmem(addr))) {return pmem_read(addr, len);} 
   IFDEF(CONFIG_DEVICE, return mmio_read(addr, len));
@@ -67,9 +69,9 @@ word_t paddr_read(paddr_t addr, int len) {
   return 0;
 }
   
-  //int pwi = 0;
+  int pwi = 0;
 void paddr_write(paddr_t addr, int len, word_t data) {
-  
+  pwi++;
   //printf("NO.paddr_write:%d\tWrite_addr = %#x\n",pwi++,addr);
   if (likely(in_pmem(addr))) { pmem_write(addr, len, data); return; } 
   IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return);
