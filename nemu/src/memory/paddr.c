e75f161909606fc1f28038eb564a0c503a089a45
tracer-ysyx <tracer@ysyx.org>
2023-07-14 21:37:33 +0800

    > compile NEMU ysyx_22050280 袁永强 Linux todas-virtual-machine 5.19.0-46-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jun 21 15:35:31 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:37:33 up  2:37,  1 user,  load average: 0.35, 0.54, 0.49

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -62,7 +62,7 @@ extern void isa_reg_display();
  
 word_t paddr_read(paddr_t addr, int len) {
   pri++;
-  if(pri > 489  && pri < 500) {printf("no.pri %d\t",pri);read_iringbuf();printf("\n");isa_reg_display(); printf("\n\n");}
+  if(pri > 445 && pri < 460) {printf("no.pri %d\t",pri);read_iringbuf();printf("\n");isa_reg_display(); printf("\n\n");}
   //printf("NO.paddr_read%d\tRead_addr  = %#x\n",pri++,addr);
   if (likely(in_pmem(addr))) {return pmem_read(addr, len);} 
   IFDEF(CONFIG_DEVICE, return mmio_read(addr, len));
