e4a3023988bf10b96edcebf9a9dd6c68b7e114ee
tracer-ysyx <tracer@ysyx.org>
2023-07-14 18:36:14 +0800

    > compile NEMU ysyx_22050280 袁永强 Linux todas-virtual-machine 5.19.0-46-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jun 21 15:35:31 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:36:14 up 19:14,  1 user,  load average: 0.73, 0.62, 0.52

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -59,7 +59,7 @@ void init_mem() {
 
 
 word_t paddr_read(paddr_t addr, int len) {
-  printf("Read_addr  = %#x\n",addr);
+  //printf("Read_addr  = %#x\n",addr);
   if (likely(in_pmem(addr))) {return pmem_read(addr, len);} 
   IFDEF(CONFIG_DEVICE, return mmio_read(addr, len));
   out_of_bound(addr);
@@ -67,7 +67,7 @@ word_t paddr_read(paddr_t addr, int len) {
 }
 
 void paddr_write(paddr_t addr, int len, word_t data) {
-  printf("Write_addr = %#x\n",addr);
+  //printf("Write_addr = %#x\n",addr);
   if (likely(in_pmem(addr))) { pmem_write(addr, len, data); return; } 
   IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return);
   out_of_bound(addr);
