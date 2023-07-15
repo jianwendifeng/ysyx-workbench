70a92db0227cd73f40a553c163216da2a0a17df5
tracer-ysyx <tracer@ysyx.org>
2023-07-15 14:45:23 +0800

    > compile NEMU ysyx_22050280 袁永强 Linux todas-virtual-machine 5.19.0-46-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jun 21 15:35:31 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:45:23 up  4:18,  1 user,  load average: 0.48, 0.30, 0.30

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -138,7 +138,6 @@ void assert_fail_msg() {
 
 /* Simulate how the CPU works. */
 void cpu_exec(uint64_t n) {
-  instr_num();
   g_print_step = (n < MAX_INST_TO_PRINT);
   switch (nemu_state.state) {
     case NEMU_END: case NEMU_ABORT:
@@ -152,7 +151,7 @@ void cpu_exec(uint64_t n) {
   
 
   execute(n);
-
+  instr_num();
 
 
   uint64_t timer_end = get_time();
