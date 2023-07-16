1c9931217e154d912da054a831febdc6fa1feaf0
tracer-ysyx <tracer@ysyx.org>
2023-07-16 14:42:35 +0800

    > compile NEMU ysyx_22050280 袁永强 Linux todas-virtual-machine 5.19.0-46-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jun 21 15:35:31 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:42:35 up 17:56,  1 user,  load average: 0.74, 0.49, 0.34

--- nemu/src/utils/ftrace.c
+++ nemu/src/utils/ftrace.c
@@ -2,20 +2,19 @@
 #include <unistd.h>
 
 extern uint64_t g_nr_guest_inst;
-FILE *elf_fp = NULL;
+FILE *ftrace_fp = NULL;
 
-void init_elf(const char *elf_file) {
-  elf_fp = stdout;
-  const char* elf_path = "/home/todas/ysyx-workbench/am-kernels/tests/cpu-tests/build";
-  if (elf_file != NULL) {
-    FILE *fp = fopen(elf_path, "w");
-    Assert(fp, "Can not open '%s'", elf_file);
-    elf_fp = fp;
+void init_ftrace(const char *ftrace_file) {
+  ftrace_fp = stdout;
+  if (ftrace_file != NULL) {
+    FILE *fp = fopen(ftrace_file, "w");
+    Assert(fp, "Can not open '%s'", ftrace_file);
+    ftrace_fp = fp;
   }
-  Log("Log is written to %s", elf_file ? elf_file : "stdout");
+  Log("Log is written to %s", ftrace_file ? ftrace_file : "stdout");
 }
 
-bool elf_enable() {
+bool ftrace_enable() {
   return MUXDEF(CONFIG_TRACE, (g_nr_guest_inst >= CONFIG_TRACE_START) &&
          (g_nr_guest_inst <= CONFIG_TRACE_END), false);
 }
