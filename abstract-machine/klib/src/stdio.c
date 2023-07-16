6b3802585ae16269e37223bfea9bd40a04c53e43
tracer-ysyx <tracer@ysyx.org>
2023-07-16 21:30:49 +0800

    > compile NEMU ysyx_22050280 袁永强 Linux todas-virtual-machine 5.19.0-46-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jun 21 15:35:31 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:30:49 up 0 min,  1 user,  load average: 1.20, 0.34, 0.12

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -109,51 +109,6 @@ int sprintf(char *out, const char *fmt, ...)
 
   return ret;
 }
-// int sprintf(char *out, const char *fmt, ...) {
-// 	va_list ap;
-//   va_start(ap, fmt);      //把参数列表拷贝到ap中,
-//   int len=0;
-//   int arg_i;
-//   char arg_i_s[64];
-//   char *arg_s;
-//   int i = 0;
-
-//   while(fmt[i] != '\0')
-//   {
-//     if(fmt[i] == '%')
-//     {
-//       i++;  //fmt指针移动到%后面
-//       switch(fmt[i])
-//       {
-//         case 's': {
-//            arg_s = va_arg(ap,char*);
-//             for(int j = 0 ; arg_s[j] != '\0' ; j++){
-//             out[len++] = arg_s[j];
-//             }
-//             i++;  //执行完成后fmt的指针再次后移
-//              break;
-//          }
-//        case 'd': {
-//            arg_i = va_arg(ap,int);  
-//            itoa(arg_i,arg_i_s);
-//            for(int j = 0 ; arg_i_s[j] != '\0' ; j++){
-//              out[len++] = arg_i_s[j];
-//            }
-//             i++;
-//            break;
-//          }
-//        default:{
-//          out[len++] = fmt[i++];
-//        }
-//       }
-//     } else{
-//       out[len++] = fmt[i++];
-//     }
-//   }
-//   out[len++] = '\0';
-//   va_end(ap);
-//   return len;
-// }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
   panic("Not implemented");
