bad73e18d89d4c9d5a4fb8841b8339e436f9460f
tracer-ysyx <tracer@ysyx.org>
2023-07-16 20:43:23 +0800

    > compile NEMU ysyx_22050280 袁永强 Linux todas-virtual-machine 5.19.0-46-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jun 21 15:35:31 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:43:23 up 23:47,  1 user,  load average: 0.42, 0.30, 0.22

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -4,123 +4,163 @@
 #include <stdarg.h>
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
-#define BUF_SIZE 1024
-// 用于反转指定字符串
-static void reserve(char *s, int len) {
-  char *end = s + len - 1;
-  char tmp;
-  while (s < end) {
-    tmp = *s;
-    *s = *end;
-    *end = tmp;
-    s++;
-    end--;
-  }
-  return;
-}
+
 // void reverse(char s[]) {
 //     int q, w;
 //     char temp;
     
-//     for (q = 0, w = strlen(s)-1; q < w; q++, w--) {
+//     for (q = 0, w = strlen(s)-1; q < w; q++, j--) {
 //         temp = s[q];
-//         s[q] = s[w];
-//         s[w] = temp;
+//         s[q] = s[j];
+//         s[j] = temp;
 //     }
 // }
 
-int itoa( int value, char *string,int radix) { // FIXME:不是标准实现
-  assert (radix <= 36);
-  int positive = value >= 0; // sign
-  int bit = 0; // 用来记录某一位的数字
-  size_t i = 0; // 用来记录当前长度的
-  if (!positive) value = -value;
-  do
-  { // FIXME:HERE
-    bit = value % radix;
-    if (bit >= 10) { string[i++] = 'a' + bit - 10; }// 16进制中大于10的表示
-    else { putstr(string); string[i++] = '0' + bit; }
-    
-  } while ((value /= radix) > 0);
-  if (!positive) string[i++] = '-';
-  string[i] = '\0';
-  reserve(string, i);
-  return i;
+// void itoa(int n,char s[]){
+//        int m = 0;
+//        do{
+//           s[m++] = n %10 +'0';
+//        }while((n /= 10) > 0);
+//        reverse(s);
+// }
+
+#define BUF_SIZE 1024
+
+
+int num_to_string(char *buffer, int num) {
+    int digits = 0;
+
+    if (num < 0) {
+        *buffer++ = '-';
+        num = -num;
+        digits++;
+    }
+
+    if (num == 0) {
+        *buffer++ = '0';
+        digits++;
+    } else {
+        int rev_num = 0;
+        while (num > 0) {
+            rev_num = rev_num * 10 + (num % 10);
+            num /= 10;
+            digits++;
+        }
+        while (rev_num > 0) {
+            *buffer++ = '0' + (rev_num % 10);
+            rev_num /= 10;
+        }
+    }
+
+    return digits;
 }
+
+
 
 int printf(const char *fmt, ...) {
   panic("Not implemented");
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
-    size_t count;
-  count = vsnprintf(out, SIZE_MAX, fmt, ap);
-  return count;
+   char *p = out;
+    const char *fmt_p = fmt;
+
+    while (*fmt_p != '\0') {
+        if (*fmt_p == '%') {
+            fmt_p++;
+            switch (*fmt_p) {
+                case 's': {
+                    char *str = va_arg(ap, char *);
+                    while (*str != '\0') {
+                        *p++ = *str++;
+                    }
+                    break;
+                }
+                case 'd': {
+                    int num = va_arg(ap, int);
+                    int digits = num_to_string(p, num);
+                    p += digits;
+                    break;
+                }
+                default:
+                    // Handle unsupported format specifier
+                    *p++ = '%'; // Write the original '%' character
+                    *p++ = *fmt_p;
+                    break;
+            }
+        } else {
+            *p++ = *fmt_p;
+        }
+        fmt_p++;
+    }
+
+    *p = '\0';
+    return p - out;
 }
 
-int sprintf(char *out, const char *fmt, ...) {
-	  // 初始化va_list
+int sprintf(char *out, const char *fmt, ...)
+{
   va_list args;
-  va_start(args, fmt);
-  size_t count;
+  int ret;
 
-  count = vsprintf(out, fmt, args);
- 
+  va_start(args,fmt);
+  ret = vsprintf(out,fmt,args);
   va_end(args);
-  return count;
-}
 
+  return ret;
+}
+// int sprintf(char *out, const char *fmt, ...) {
+// 	va_list ap;
+//   va_start(ap, fmt);      //把参数列表拷贝到ap中,
+//   int len=0;
+//   int arg_i;
+//   char arg_i_s[64];
+//   char *arg_s;
+//   int i = 0;
+
+//   while(fmt[i] != '\0')
+//   {
+//     if(fmt[i] == '%')
+//     {
+//       i++;  //fmt指针移动到%后面
+//       switch(fmt[i])
+//       {
+//         case 's': {
+//            arg_s = va_arg(ap,char*);
+//             for(int j = 0 ; arg_s[j] != '\0' ; j++){
+//             out[len++] = arg_s[j];
+//             }
+//             i++;  //执行完成后fmt的指针再次后移
+//              break;
+//          }
+//        case 'd': {
+//            arg_i = va_arg(ap,int);  
+//            itoa(arg_i,arg_i_s);
+//            for(int j = 0 ; arg_i_s[j] != '\0' ; j++){
+//              out[len++] = arg_i_s[j];
+//            }
+//             i++;
+//            break;
+//          }
+//        default:{
+//          out[len++] = fmt[i++];
+//        }
+//       }
+//     } else{
+//       out[len++] = fmt[i++];
+//     }
+//   }
+//   out[len++] = '\0';
+//   va_end(ap);
+//   return len;
+// }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
   panic("Not implemented");
 }
 
 int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
-  assert(NULL != out);
-  assert(NULL != fmt);
-
-  size_t count;
-  size_t fmt_index;
-
-  // 解析fmt + 解析...参数
-  for (count = 0, fmt_index = 0; fmt[fmt_index] != '\0' && count < n;)
-  {
-    if (fmt[fmt_index] != '%') { // 不是要求匹配
-      out[count] = fmt[fmt_index];
-      count++;
-      fmt_index++;
-    } else {
-      switch (fmt[fmt_index+1])
-      {
-      case '%':
-        out[count] = fmt[fmt_index];
-        count++;
-	      fmt_index += 2;
-        break;
-      case 'd':
-        count += itoa(va_arg(ap, int), out+count, 10);
-        fmt_index += 2;
-        break;
-      case 's':
-        strcpy(out+count, va_arg(ap, char*));
-        count += strlen(out + count);
-        fmt_index += 2;
-        break;
-      case 'c':
-        out[count] = (char)va_arg(ap, int);
-        count++;
-        fmt_index += 2;
-        break;
-      default:
-        out[count] = fmt[fmt_index];
-	    	out[count+1] = fmt[fmt_index+1];
-	    	count += 2;
-	    	fmt_index += 2;
-        break;
-      }
-    }
-  }
-  out[count] = '\0';
-  return count;
+  panic("Not implemented");
 }
+
 #endif	
