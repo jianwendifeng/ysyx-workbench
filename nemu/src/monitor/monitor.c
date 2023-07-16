df14fafff7ae8a04743b89edf38e3fe9933303f6
tracer-ysyx <tracer@ysyx.org>
2023-07-16 14:40:08 +0800

    > compile NEMU ysyx_22050280 袁永强 Linux todas-virtual-machine 5.19.0-46-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jun 21 15:35:31 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:40:08 up 17:54,  1 user,  load average: 0.38, 0.33, 0.28

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -18,7 +18,7 @@
 
 void init_rand();
 void init_log(const char *log_file);
-void init_elf(const char *elf_file);
+void init_ftrace(const char *elf_file);
 void init_mem();
 void init_difftest(char *ref_so_file, long img_size, int port);
 void init_device();
@@ -45,7 +45,7 @@ void sdb_set_batch_mode();
 static char *log_file = NULL;
 static char *diff_so_file = NULL;
 static char *img_file = NULL;
-static char *elf_file =NULL;
+static char *ftrace_file =NULL;
 static int difftest_port = 1234;
 
 static long load_img() {
@@ -74,7 +74,7 @@ static int parse_args(int argc, char *argv[]) {
   const struct option table[] = {
     {"batch"    , no_argument      , NULL, 'b'},
     {"log"      , required_argument, NULL, 'l'},
-    {"elf"      , required_argument, NULL, 'e'},
+    {"ftrace"   , required_argument, NULL, 'f'},
     {"diff"     , required_argument, NULL, 'd'},
     {"port"     , required_argument, NULL, 'p'},
     {"help"     , no_argument      , NULL, 'h'},
@@ -87,7 +87,7 @@ static int parse_args(int argc, char *argv[]) {
       case 'p': sscanf(optarg, "%d", &difftest_port); break;
       case 'l': log_file = optarg; break;
       case 'd': diff_so_file = optarg; break;
-      case 'e': elf_file = optarg; break;
+      case 'e': ftrace_file = optarg; break;
       case 1: img_file = optarg; return 0;
       default:
         printf("Usage: %s [OPTION...] IMAGE [args]\n\n", argv[0]);
@@ -114,8 +114,8 @@ void init_monitor(int argc, char *argv[]) {
   /* Open the log file. */
   init_log(log_file);
 
-  /* Open the elf file. */
-  init_elf(elf_file);
+  /* Initialize ftrace. */
+  init_ftrace(ftrace_file);
 
   /* Initialize memory. */
   init_mem();
