8e9341ad703562b8ce482a75a0c0d652725090c8
tracer-ysyx <tracer@ysyx.org>
2023-07-19 12:21:07 +0800

    > compile NEMU ysyx_22050280 袁永强 Linux todas-virtual-machine 5.19.0-46-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jun 21 15:35:31 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:21:07 up 18:58,  1 user,  load average: 0.48, 0.36, 0.25

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -4,6 +4,40 @@
 #include <stdarg.h>
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
+#define BUF_SIZE 1024
+
+// 用于反转指定字符串
+static void reserve(char *s, int len) {
+  char *end = s + len - 1;
+  char tmp;
+  while (s < end) {
+    tmp = *s;
+    *s = *end;
+    *end = tmp;
+    s++;
+    end--;
+  }
+  return;
+}
+
+int itoa( int value, char *string,int radix) { // FIXME:不是标准实现
+  assert (radix <= 36);
+  int positive = value >= 0; // sign
+  int bit = 0; // 用来记录某一位的数字
+  size_t i = 0; // 用来记录当前长度的
+  if (!positive) value = -value;
+  do
+  { // FIXME:HERE
+    bit = value % radix;
+    if (bit >= 10) { string[i++] = 'a' + bit - 10; }// 16进制中大于10的表示
+    else { putstr(string); string[i++] = '0' + bit; }
+    
+  } while ((value /= radix) > 0);
+  if (!positive) string[i++] = '-';
+  string[i] = '\0';
+  reserve(string, i);
+  return i;
+}
 
 // void reverse(char s[]) {
 //     int q, w;
@@ -61,61 +95,76 @@ int printf(const char *fmt, ...) {
   panic("Not implemented");
 }
 
-int vsprintf(char *out, const char *fmt, va_list ap) {
-   char *p = out;
-    const char *fmt_p = fmt;
+int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
+  assert(NULL != out);
+  assert(NULL != fmt);
 
-    while (*fmt_p != '\0') {
-        if (*fmt_p == '%') {
-            fmt_p++;
-            switch (*fmt_p) {
-                case 's': {
-                    char *str = va_arg(ap, char *);
-                    while (*str != '\0') {
-                        *p++ = *str++;
-                    }
-                    break;
-                }
-                case 'd': {
-                    int num = va_arg(ap, int);
-                    int digits = num_to_string(p, num);
-                    p += digits;
-                    break;
-                }
-                default:
-                    // Handle unsupported format specifier
-                    *p++ = '%'; // Write the original '%' character
-                    *p++ = *fmt_p;
-                    break;
-            }
-        } else {
-            *p++ = *fmt_p;
-        }
-        fmt_p++;
+  int count;
+  int fmt_index;
+
+  for (count = 0, fmt_index = 0; fmt[fmt_index] != '\0' && count < n;)
+  {
+    if (fmt[fmt_index] != '%') { 
+      out[count] = fmt[fmt_index];
+      count++;
+      fmt_index++;
+    } else {
+      switch (fmt[fmt_index+1])
+      {
+      case '%':
+        out[count] = fmt[fmt_index];
+        count++;
+	      fmt_index += 2;
+        break;
+      case 'd':
+        count += itoa(va_arg(ap, int), out+count, 10);
+        fmt_index += 2;
+        break;
+      case 's':
+        strcpy(out+count, va_arg(ap, char*));
+        count += strlen(out + count);
+        fmt_index += 2;
+        break;
+      case 'c':
+        out[count] = (char)va_arg(ap, int);
+        count++;
+        fmt_index += 2;
+        break;
+      default:
+        out[count] = fmt[fmt_index];
+	    	out[count+1] = fmt[fmt_index+1];
+	    	count += 2;
+	    	fmt_index += 2;
+        break;
+      }
     }
+  }
+  out[count] = '\0';
+  return count;
+}
 
-    *p = '\0';
-    return p - out;
+int vsprintf(char *out, const char *fmt, va_list ap) {
+    int count = 0;
+    count = vsnprintf(out, SIZE_MAX, fmt, ap);
+    return count;
 }
 
 int sprintf(char *out, const char *fmt, ...)
 {
   va_list args;
-  int ret;
+  int len = 0;
 
   va_start(args,fmt);
-  ret = vsprintf(out,fmt,args);
+  len = vsprintf(out,fmt,args);
   va_end(args);
 
-  return ret;
+  return len;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
   panic("Not implemented");
 }
 
-int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
-  panic("Not implemented");
-}
+
 
 #endif	
