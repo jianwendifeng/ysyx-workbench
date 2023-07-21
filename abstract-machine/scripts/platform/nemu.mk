5de9afc9d469094571229127e60bb8af5f9ec00c
tracer-ysyx <tracer@ysyx.org>
2023-07-21 16:24:14 +0800

    > compile NEMU ysyx_22050280 袁永强 Linux todas-virtual-machine 5.19.0-46-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jun 21 15:35:31 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:24:14 up 1 day, 17:39,  1 user,  load average: 0.33, 0.35, 0.36

--- abstract-machine/scripts/platform/nemu.mk
+++ abstract-machine/scripts/platform/nemu.mk
@@ -12,7 +12,8 @@ LDFLAGS   += -T $(AM_HOME)/scripts/linker.ld \
              --defsym=_pmem_start=0x80000000 --defsym=_entry_offset=0x0
 LDFLAGS   += --gc-sections -e _start
 NEMUFLAGS += 	-b -l    $(shell dirname $(IMAGE).elf)/nemu-log.txt                #批处理-b  
-NEMUFLAGS += -e     $(shell dirname $(IMAGE).elf).elf
+NEMUFLAGS += -e     $(/home/todas/ysyx-workbench/am-kernels/tests/cpu-tests/build)/$(IMAGE).elf
+).elf
 
 CFLAGS += -DMAINARGS=\"$(mainargs)\"
 CFLAGS += -I$(AM_HOME)/am/src/platform/nemu/include
