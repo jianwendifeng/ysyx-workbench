9c688bfcdca89a7694b01edabc2180b9fa38fd75
tracer-ysyx <tracer@ysyx.org>
2023-07-21 16:46:04 +0800

    > compile NEMU ysyx_22050280 袁永强 Linux todas-virtual-machine 5.19.0-46-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jun 21 15:35:31 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:46:04 up 1 day, 18:01,  1 user,  load average: 0.43, 0.51, 0.44

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -82,7 +82,7 @@ static int parse_args(int argc, char *argv[]) {
   };
   int o;
   while ( (o = getopt_long(argc, argv, "-bhl:d:p:", table, NULL)) != -1) {
-    printf("o:%d\n\n",o);
+    printf("o:%c\n\n",o);
     switch (o) {
       case 'b': sdb_set_batch_mode(); break;
       case 'p': sscanf(optarg, "%d", &difftest_port); break;
