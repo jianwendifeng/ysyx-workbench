383e6e6c639edc7f597e22ae420565472edf1d09
tracer-ysyx <tracer@ysyx.org>
2023-07-21 16:45:22 +0800

    > compile NEMU ysyx_22050280 袁永强 Linux todas-virtual-machine 5.19.0-46-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jun 21 15:35:31 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:45:22 up 1 day, 18:01,  1 user,  load average: 0.74, 0.56, 0.45

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -82,6 +82,7 @@ static int parse_args(int argc, char *argv[]) {
   };
   int o;
   while ( (o = getopt_long(argc, argv, "-bhl:d:p:", table, NULL)) != -1) {
+     printf("Usage: %s [OPTION...] IMAGE [args]\n\n", argv[0]);
     switch (o) {
       case 'b': sdb_set_batch_mode(); break;
       case 'p': sscanf(optarg, "%d", &difftest_port); break;
