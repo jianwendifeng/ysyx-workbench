65a84b9a914b61fe3000be2060876124cdc2763d
tracer-ysyx <tracer@ysyx.org>
2023-07-22 16:12:56 +0800

    > compile NEMU ysyx_22050280 袁永强 Linux todas-virtual-machine 5.19.0-46-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jun 21 15:35:31 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:12:56 up 2 days,  5:39,  1 user,  load average: 0.48, 0.62, 0.62

--- nemu/src/isa/riscv64/inst.c
+++ nemu/src/isa/riscv64/inst.c
@@ -58,10 +58,6 @@ static int decode_exec(Decode *s) {
   word_t src1 = 0, src2 = 0, imm = 0;	//word_t uint64_t;	sword_t int64_t
   s->dnpc = s->snpc;
 
-  #ifdef CONFIG_ITRACE
-       write_iringbuf(*s);  //iringbuf
-    #endif
-
 #define INSTPAT_INST(s) ((s)->isa.inst.val)
 #define INSTPAT_MATCH(s,  name,  type, ... /* execute body */ ) { \
   decode_operand(s, &rd, &src1, &src2, &imm, concat(TYPE_, type)); \
@@ -143,6 +139,9 @@ static int decode_exec(Decode *s) {
 
   	R(0) = 0; // reset $zero to 0
 	
+	  #ifdef CONFIG_ITRACE
+       write_iringbuf(*s);  //iringbuf
+    #endif
 
 
   	return 0;
@@ -153,7 +152,5 @@ static int decode_exec(Decode *s) {
 
 int isa_exec_once(Decode *s) {
   s->isa.inst.val = inst_fetch(&s->snpc, 4);
-
-
   return decode_exec(s);
 }
