b47b92612dd755ff6241264d24582e01196645a7
tracer-ysyx <tracer@ysyx.org>
2023-07-22 14:28:29 +0800

    > compile NEMU ysyx_22050280 袁永强 Linux todas-virtual-machine 5.19.0-46-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jun 21 15:35:31 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:28:29 up 2 days,  3:55,  1 user,  load average: 0.79, 0.55, 0.45

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -3,16 +3,14 @@
 #include <stdio.h>
 
 #define KEYDOWN_MASK 0x8000
-// #define KEY_NONE 0
+#define KEY_NONE 0
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
   // kbd->keydown = 0;
   // kbd->keycode = AM_KEY_NONE;
   int kb = inl(KBD_ADDR);
   kbd->keydown = (kb & KEYDOWN_MASK ? true : false); //1000000000000000 true
-  if (kbd->keydown){printf("keydown%d\n",kbd->keydown);}
 
-
-  kbd->keycode = kb & ~KEYDOWN_MASK ;   //清除键盘扫描码中表示按键按下状态的最高位。
+  kbd->keycode = (kbd->keydown) ? kb & ~KEYDOWN_MASK : KEY_NONE ;   //清除键盘扫描码中表示按键按下状态的最高位。
 
 }
