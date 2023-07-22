58ce17603816f939aba563a7380f1432aa3dc7ad
tracer-ysyx <tracer@ysyx.org>
2023-07-22 11:14:38 +0800

    > compile NEMU ysyx_22050280 袁永强 Linux todas-virtual-machine 5.19.0-46-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jun 21 15:35:31 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:14:38 up 2 days,  2:05,  1 user,  load average: 0.54, 0.67, 0.57

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -33,17 +33,10 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
   int x = ctl->x , y = ctl->y , w = ctl->w , h = ctl->h;
   uint32_t* pixels = (uint32_t *)ctl->pixels;
   uint32_t* fb = (uint32_t *)(uintptr_t)FB_ADDR;
-  //if((!w) || (!h)) return ;
-
-// for(int j = 0; j< h; j++){
-//     for(int i = 0; i< w; i++){
-//       fb[(y+j)*W+(x+i)] = *(pixels+j*w+i);
-//     }
-//   }
 
   for(int i=0; i < h ; i++){          //i is height
     for(int j = 0 ; j <w ; j++){     //j is weight
-      fb[x+j+(y+i)*W] = pixels[w*i+j];
+      fb[(x + j) + (y + i)*W] = pixels[(w*i) + j];
     }
   }
 
