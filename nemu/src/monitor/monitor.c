af665a2bb8a9a4f0e15e4d7acf533cf67a491301
22050280-Yuan Yongqiang <2362526092@qq.com>
2023-07-25 15:40:23 +0800

    ftrace实现

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -18,6 +18,7 @@
 
 void init_rand();
 void init_log(const char *log_file);
+void init_ftrace(const char *ftrace_file);
 void init_mem();
 void init_difftest(char *ref_so_file, long img_size, int port);
 void init_device();
@@ -44,6 +45,7 @@ void sdb_set_batch_mode();
 static char *log_file = NULL;
 static char *diff_so_file = NULL;
 static char *img_file = NULL;
+static char *elf_file =NULL;
 static int difftest_port = 1234;
 
 static long load_img() {
@@ -73,16 +75,18 @@ static int parse_args(int argc, char *argv[]) {
     {"batch"    , no_argument      , NULL, 'b'},
     {"log"      , required_argument, NULL, 'l'},
     {"diff"     , required_argument, NULL, 'd'},
+    {"elf"      , required_argument, NULL, 'e'},
     {"port"     , required_argument, NULL, 'p'},
     {"help"     , no_argument      , NULL, 'h'},
     {0          , 0                , NULL,  0 },
   };
   int o;
-  while ( (o = getopt_long(argc, argv, "-bhl:d:p:", table, NULL)) != -1) {
+  while ( (o = getopt_long(argc, argv, "-bhl:e:d:p:", table, NULL)) != -1) {
     switch (o) {
       case 'b': sdb_set_batch_mode(); break;
       case 'p': sscanf(optarg, "%d", &difftest_port); break;
       case 'l': log_file = optarg; break;
+      case 'e': elf_file = optarg; break;
       case 'd': diff_so_file = optarg; break;
       case 1: img_file = optarg; return 0;
       default:
@@ -91,6 +95,7 @@ static int parse_args(int argc, char *argv[]) {
         printf("\t-l,--log=FILE           output log to FILE\n");
         printf("\t-d,--diff=REF_SO        run DiffTest with reference REF_SO\n");
         printf("\t-p,--port=PORT          run DiffTest with port PORT\n");
+        printf("\t-e,--elf=FILE           run ftrace\n");
         printf("\n");
         exit(0);
     }
@@ -107,8 +112,12 @@ void init_monitor(int argc, char *argv[]) {
   /* Set random seed. */
   init_rand();
 
+    
   /* Open the log file. */
   init_log(log_file);
+
+    /* Initialize ftrace. */
+  init_ftrace(elf_file);
 
   /* Initialize memory. */
   init_mem();
@@ -125,6 +134,8 @@ void init_monitor(int argc, char *argv[]) {
   /* Initialize differential testing. */
   init_difftest(diff_so_file, img_size, difftest_port);
 
+
+  
   /* Initialize the simple debugger. */
   init_sdb();
 
