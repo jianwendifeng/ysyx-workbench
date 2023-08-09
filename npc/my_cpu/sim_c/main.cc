46c9292e596951a94686ea30db07832765d9e513
22050280-Yuan Yongqiang <2362526092@qq.com>
2023-08-09 19:33:01 +0800

    OpenXiangShan/chisel-playground ysyx2204 initialized
    OpenXiangShan/chisel-playground fa5d392cfd99bbe1584b1cec60bc5679c66ea5c7 trace code

--- npc/my_cpu/sim_c/main.cc
+++
@@ -1,8 +0,0 @@
-#include "sim.h"
-
-int main(int argc, char *argv[]) {
-
-  sim_main(argc, argv);
-
-  return 0;
-}
--- npc/my_cpu/src/main/scala/rv64/config.scala
+++
@@ -1,7 +0,0 @@
-// package rv64
-
-// import junctions.NastiBundleParameters
-
-// object coreconfig {
-//     def apply(): Config
-// }
